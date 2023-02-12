1d923a257832388e9e93819accf57355f35a9639
huanhutlinh <huanhutlinh128@gmail.com>
2023-02-12 16:50:57 +0700

    React Router Setup

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+//rafce
+import React from "react";
+
+import "./style.scss";
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+export default Footer;
---
+++ src/pages/searchResult/SearchResult.jsx
@@ -0,0 +1,10 @@
+///rafce
+import React from "react";
+
+import "./style.scss";
+
+const SearchResult = () => {
+  return <div>SearchResult</div>;
+};
+
+export default SearchResult;
