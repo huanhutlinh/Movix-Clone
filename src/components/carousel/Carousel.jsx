ba70e5d1c6af5ee7f8fac657cdcd4bf14edd6ba6
huanhutlinh <huanhutlinh128@gmail.com>
2023-02-14 20:06:55 +0700

    Handle Sliding Left and Right

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -21,7 +21,19 @@ const Carousel = ({ data, loading }) => {
   const { url } = useSelector((state) => state.home);
   const navigate = useNavigate();
 
-  const navigation = (dir) => {};
+  const navigation = (dir) => {
+    const container = carouselContainer.current;
+
+    const scrollAmount =
+      dir === "left"
+        ? container.scrollLeft - (container.offsetWidth + 20)
+        : container.scrollLeft + (container.offsetWidth + 20);
+
+    container.scrollTo({
+      left: scrollAmount,
+      behavior: "smooth",
+    });
+  };
 
   const skItem = () => {
     return (
@@ -41,21 +53,25 @@ const Carousel = ({ data, loading }) => {
       <ContentWrapper>
         <BsFillArrowLeftCircleFill
           className="carouselLeftNav arrow"
-          onClick={() => navigate("left")}
+          onClick={() => navigation("left")}
         />
         <BsFillArrowRightCircleFill
           className="carouselRightNav arrow"
-          onClick={() => navigate("right")}
+          onClick={() => navigation("right")}
         />
         {!loading ? (
-          <div className="carouselItems">
+          <div className="carouselItems" ref={carouselContainer}>
             {data?.map((item) => {
               const posterUrl = item.poster_path
                 ? url.poster + item.poster_path
                 : PosterFallback;
 
               return (
-                <div key={item.id} className="carouselItem">
+                <div
+                  key={item.id}
+                  className="carouselItem"
+                  onClick={() => navigate(`/${item.media_type}/${item.id}`)}
+                >
                   <div className="posterBlock">
                     <Img src={posterUrl} />
                     <CircleRating rating={item.vote_average.toFixed(1)} />
