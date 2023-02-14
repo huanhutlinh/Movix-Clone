d0c4fc4ac0e77db49e5e1d1cf695dadef35d3704
huanhutlinh <huanhutlinh128@gmail.com>
2023-02-14 13:35:58 +0700

    Main Carousel Component: Render Layout

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import Carousel from "../../../components/carousel/Carousel";
 
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
@@ -21,6 +22,7 @@ const Trending = () => {
         <span className="carouselTitle">Trending</span>
         <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
       </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} />
     </div>
   );
 };
